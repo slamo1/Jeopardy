1b27afb042f50b0fa15b289e64c0b489a9a56949
Jessica Nash <janash@vt.edu>
2019-10-17 23:44:26 -0400

    update host set up to use jinja to build form

--- main.py
+++ main.py
@@ -17,7 +17,16 @@ CSS under static
 """
 @app.route("/host_setup", methods=['GET', 'POST'])
 def host_setup():
-    return render_template("host_setup.html")
+    categories = range(1,7)
+    
+    start_amount = 100
+    stop_amount = 700
+    increment = 100
+    jeopardy = range(start_amount, stop_amount, increment)
+    double_jeopardy = range(2*start_amount, 2*stop_amount, 2*increment)
+    
+    return render_template("host_setup.html", categories=categories, \
+        jeopardy=jeopardy, double_jeopardy=double_jeopardy)
 
 if __name__ == "__main__":
     app.run(debug=True)
