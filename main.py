99115adac2ce9ccc7403d20367f139831287b835
Jessica Nash <janash@vt.edu>
2019-10-23 20:08:41 -0400

    fix jeopardy board route, add routes for questions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route("/")
 def home():
@@ -13,21 +15,22 @@ CSS under static
             |    |
         HOST      PLAYER
 """
-@app.route("/host_setup")
+@app.route("/host_setup", methods=['GET', 'POST'])
 def host_setup():
-    categories = range(1,7)
 
+    categories = range(1,7)
     start_amount = 100
     stop_amount = 700
     increment = 100
-    jeopardy = range(start_amount, stop_amount, increment)
-    double_jeopardy = range(2*start_amount, 2*stop_amount, 2*increment)
+    
+    if request.method == 'GET':
+    
+        jeopardy = range(start_amount, stop_amount, increment)
+        double_jeopardy = range(2*start_amount, 2*stop_amount, 2*increment)
 
-    return render_template("host_setup.html", categories=categories, \
-        jeopardy=jeopardy, double_jeopardy=double_jeopardy)
-
-@app.route("/host_setup", methods=['POST'])
-def jeopardy_form_post():
+        return render_template("host_setup.html", categories=categories, \
+            jeopardy=jeopardy, double_jeopardy=double_jeopardy)
+    
     if request.method == "POST":
         categories = []
         answers = []
@@ -36,23 +39,35 @@ def jeopardy_form_post():
         for i in range(6):
             cat_str ="c"+ str(i+1)
             categories.append(request.form.get(cat_str))
+            
             for n in range(6):
                 que_str = "c"+ str(i+1) + "-" + str((n+1)*100)
                 ans_str = "nm" + str(i+1) + "-" + str((n+1)*100)
                 questions.append(request.form.get(que_str))
                 answers.append(request.form.get(ans_str))
-        print(categories)
-        print(questions)
-        print(answers)
-        start_amount = 100
-        stop_amount = 700
-        increment = 100
-        increments = range(start_amount, stop_amount, increment)
-        return render_template("jeo_board.html", categories=categories, \
-            answers=answers, questions=questions, increments=increments)
+        
+        increments = list(range(start_amount, stop_amount, increment))
 
-@app.route("/answer_page")
-def answer_page():
-    return render_template("answer_page.html")
+        session['categories'] = categories
+        session['questions'] = questions
+        session['answers'] = answers
+        session['increments'] = increments
+        
+        return redirect(url_for('jeopardy_form_post'))
+
+
+@app.route("/jeopardy_board", methods=['GET', 'POST'])
+def jeopardy_form_post():
+    return render_template("jeo_board.html", categories=session['categories'], \
+            answers=session['answers'], questions=session['questions'], increments=session['increments'])
+
+
+    
+@app.route("/answer_page/<category>/<value>")
+def answer_page(category, value):
+    return render_template("answer_page.html", category=category, value=value)
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
