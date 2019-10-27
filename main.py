35aebdfdef9d5d7595718c5f24ab89943e72cc85
Connor Slamowitz <slamo1@vt.edu>
2019-10-27 16:11:15 -0400

    Answer Page Data Structure

--- main.py
+++ main.py
@@ -22,50 +22,56 @@ def host_setup():
     start_amount = 100
     stop_amount = 700
     increment = 100
-    
+
     if request.method == 'GET':
-    
+
         jeopardy = range(start_amount, stop_amount, increment)
         double_jeopardy = range(2*start_amount, 2*stop_amount, 2*increment)
 
         return render_template("host_setup.html", categories=categories, \
             jeopardy=jeopardy, double_jeopardy=double_jeopardy)
-    
+
     if request.method == "POST":
         categories = []
-        answers = []
-        questions = []
+        increments = []
 
+        master = []
         for i in range(6):
             cat_str ="c"+ str(i+1)
             categories.append(request.form.get(cat_str))
-            
-            for n in range(6):
-                que_str = "c"+ str(i+1) + "-" + str((n+1)*100)
-                ans_str = "nm" + str(i+1) + "-" + str((n+1)*100)
-                questions.append(request.form.get(que_str))
-                answers.append(request.form.get(ans_str))
-        
-        increments = list(range(start_amount, stop_amount, increment))
 
+            jeop_board = {}
+            for n in range(6):
+                val = (n+1)*100
+                increments.append(val)
+                que_str = "c"+ str(i+1) + "-" + str(val)
+                ans_str = "nm" + str(i+1) + "-" + str(val)
+                jeop_board[val] = [request.form.get(que_str), request.form.get(ans_str)]
+            master.append(jeop_board)
+                #questions.append(request.form.get(que_str))
+                #answers.append(request.form.get(ans_str))
+        print(master)
+        increments = list(range(start_amount, stop_amount, increment))
+        session['jeopardy'] = master
         session['categories'] = categories
-        session['questions'] = questions
-        session['answers'] = answers
         session['increments'] = increments
-        
+
         return redirect(url_for('jeopardy_form_post'))
 
 
 @app.route("/jeopardy_board", methods=['GET', 'POST'])
 def jeopardy_form_post():
     return render_template("jeo_board.html", categories=session['categories'], \
-            answers=session['answers'], questions=session['questions'], increments=session['increments'])
+            increments=session['increments'])
 
 
-    
+
 @app.route("/answer_page/<category>/<value>")
 def answer_page(category, value):
-    return render_template("answer_page.html", category=category, value=value)
+    category = int(category)
+    question = session['jeopardy'][category][value][0]
+    answer = session['jeopardy'][category][value][1]
+    return render_template("answer_page.html", question=question, answer=answer)
 
 
 
